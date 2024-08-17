849ef8c0321bb71ce3055ad4eeb423aa4132417c
anshuER <anshupatel710@gmail.com>
2024-08-17 16:21:24 +0530

    image path change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Header = () => {
   return (
     <div className='flex justify-between'>
-      <img className='w-[227px] h-[50px]' src='/image.png' alt='logo' />
+      <img
+        className='w-[227px] h-[50px]'
+        src='./test-thence/image.png'
+        alt='logo'
+      />
       <div className='flex gap-8 items-center text-xl font-medium'>
         <p className='text-red-500'>Home</p>
         <p>Pages</p>
